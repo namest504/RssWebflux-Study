44e1a0f67d5b5b785230baff02b23a71dbf78680
List <61047602+namest504@users.noreply.github.com>
2023-07-04 08:24:13 +0900

    init

---
+++ src/test/java/com/list/realtimenews/RealTimeNewsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.list.realtimenews;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class RealTimeNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
