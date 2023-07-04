d2bd1eec5f6c53df3bf0848ce6f45479c8b0d372
List <61047602+namest504@users.noreply.github.com>
2023-07-05 05:58:06 +0900

    feat: 데이터 저장 API

--- src/main/java/com/list/realtimenews/controller/RssController.java
+++ src/main/java/com/list/realtimenews/controller/RssController.java
@@ -2,10 +2,15 @@ package com.list.realtimenews.controller;
 
 import com.list.realtimenews.dto.RssDto.RssRootResponse;
 import com.list.realtimenews.service.RssService;
+import entity.RssChannel;
+import entity.RssImage;
+import entity.RssItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 public class RssController {
@@ -17,9 +22,21 @@ public class RssController {
         this.rssService = rssService;
     }
 
-    @GetMapping("/")
-    public Mono<RssRootResponse> getRssData() {
+    @GetMapping("/get")
+    public Mono<RssRootResponse> getRssDataInJson() {
         Mono<String> rssXML = rssService.getRssXML();
         return rssService.convertXMLToPOJO(rssXML);
+    }
+
+    @GetMapping("/save")
+    public String saveRssData() {
+        Mono<RssRootResponse> rssRootResponseMono = rssService.convertXMLToPOJO(rssService.getRssXML());
+        Mono<RssChannel> rssChannel = rssService.getRssChannel(rssRootResponseMono);
+        Mono<RssImage> rssImage = rssService.getRssImage(rssRootResponseMono);
+        Mono<List<RssItem>> rssItem = rssService.getRssItem(rssRootResponseMono);
+        rssService.saveRssChannel(rssChannel).subscribe();
+        rssService.saveRssImage(rssImage).subscribe();
+        rssService.saveRssItem(rssItem).subscribe();
+        return "true";
     }
 }
