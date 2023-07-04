c0949f31cde5dba89a7e9fbeb90edb24abf6a153
List <61047602+namest504@users.noreply.github.com>
2023-07-05 05:57:10 +0900

    feat: RSS 데이터 저장 로직

--- src/main/java/com/list/realtimenews/service/RssService.java
+++ src/main/java/com/list/realtimenews/service/RssService.java
@@ -2,26 +2,46 @@ package com.list.realtimenews.service;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.list.realtimenews.dto.RssDto;
+import com.list.realtimenews.dto.RssDto.Item;
 import com.list.realtimenews.dto.RssDto.RssRootResponse;
+import com.list.realtimenews.repository.RssChannelRepository;
+import com.list.realtimenews.repository.RssImageRepository;
+import com.list.realtimenews.repository.RssItemRepository;
+import entity.RssChannel;
+import entity.RssImage;
+import entity.RssItem;
+import lombok.extern.slf4j.Slf4j;
 import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
+import java.util.List;
 
+import static entity.RssItem.convertToRssItems;
 
+@Slf4j
 @Service
 public class RssService {
 
     private WebClient webClient;
     private ObjectMapper objectMapper;
+    private RssChannelRepository rssChannelRepository;
+    private RssImageRepository rssImageRepository;
+    private RssItemRepository rssItemRepository;
 
     @Autowired
-    public RssService(WebClient webClient, ObjectMapper objectMapper) {
+    public RssService(WebClient webClient, ObjectMapper objectMapper, RssChannelRepository rssChannelRepository, RssImageRepository rssImageRepository, RssItemRepository rssItemRepository) {
         this.webClient = webClient;
         this.objectMapper = objectMapper;
+        this.rssChannelRepository = rssChannelRepository;
+        this.rssImageRepository = rssImageRepository;
+        this.rssItemRepository = rssItemRepository;
+
     }
 
     public Mono<String> getRssXML() {
@@ -43,5 +63,62 @@ public class RssService {
                 return Mono.error(new RuntimeException(e));
             }
         });
+    }
+
+    public Mono<RssChannel> getRssChannel(Mono<RssRootResponse> rssRootResponseMono) {
+        return rssRootResponseMono.flatMap( x -> Mono.just(RssChannel.of(x.getRss().getChannel())));
+    }
+
+    public Mono<RssImage> getRssImage(Mono<RssRootResponse> rssRootResponseMono) {
+        return rssRootResponseMono.flatMap(x -> {
+            RssDto.Image image = x.getRss().getChannel().getImage();
+            if (image != null) {
+                return Mono.just(RssImage.of(image));
+            } else {
+                return Mono.empty();
+            }
+        }).onErrorResume(e -> {
+            return Mono.error(new Exception(e));
+        });
+    }
+
+    public Mono<List<RssItem>> getRssItem(Mono<RssRootResponse> rssRootResponseMono) {
+        return rssRootResponseMono.map(r -> {
+            List<Item> items = r.getRss().getChannel().getItem();
+            List<RssItem> rssItems = convertToRssItems(items);
+            return rssItems;
+        });
+    }
+
+    public Mono<RssChannel> saveRssChannel(Mono<RssChannel> rssChannel) {
+        log.info("saveRssChannel 실행");
+        return rssChannel.flatMap( r ->
+                rssChannelRepository.save(RssChannel.builder()
+                            .title(r.getTitle())
+                            .link(r.getLink())
+                            .description(r.getDescription())
+                            .language(r.getLanguage())
+                            .pubDate(r.getPubDate())
+                            .lastBuildDate(r.getLastBuildDate())
+                            .build()));
+    }
+
+    public Mono<RssImage> saveRssImage(Mono<RssImage> rssImage) {
+        log.info("saveRssImage 실행");
+        return rssImage.flatMap( r ->
+            rssImageRepository.save(RssImage.builder()
+                            .title(r.getTitle())
+                            .url(r.getUrl())
+                            .link(r.getLink())
+                            .description(r.getDescription())
+                    .build())
+        );
+    }
+
+    public Flux<RssItem> saveRssItem(Mono<List<RssItem>> rssItems) {
+        log.info("saveRssItem 실행");
+        return rssItems.flatMapMany(Flux::fromIterable)
+                .flatMap( r -> rssItemRepository.save(r));
+
     }
 }
