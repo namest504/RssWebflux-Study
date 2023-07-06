4f210f1d357b5c400787b047dcdb1f4ffbe91f0d
List <61047602+namest504@users.noreply.github.com>
2023-07-06 16:15:56 +0900

    refactor: 코드 리팩토링

--- src/main/java/com/list/realtimenews/controller/RssController.java
+++ src/main/java/com/list/realtimenews/controller/RssController.java
@@ -1,19 +1,12 @@
 package com.list.realtimenews.controller;
 
 import com.list.realtimenews.dto.RssDto.RssData;
-import com.list.realtimenews.dto.RssDto.RssRootResponse;
 import com.list.realtimenews.handler.RssHandler;
 import com.list.realtimenews.service.RssService;
-import entity.RssChannel;
-import entity.RssImage;
-import entity.RssItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @RestController
 public class RssController {
@@ -27,23 +20,23 @@ public class RssController {
         this.rssHandler = rssHandler;
     }
 
-    @GetMapping("/get")
-    public Mono<RssRootResponse> getRssDataInJson() {
-        Mono<String> rssXML = rssService.getRssXML();
-        return rssService.convertXMLToPOJO(rssXML);
-    }
+//    @GetMapping("/get")
+//    public Mono<RssRootResponse> getRssDataInJson() {
+//        Mono<List<String>> rssXML = rssService.getRssXML();
+//        return rssService.convertXMLToPOJO(rssXML);
+//    }
 
-    @GetMapping("/save")
-    public String saveRssData() {
-        Mono<RssRootResponse> rssRootResponseMono = rssService.convertXMLToPOJO(rssService.getRssXML());
-        Mono<RssChannel> rssChannel = rssService.getRssChannel(rssRootResponseMono);
-        Mono<RssImage> rssImage = rssService.getRssImage(rssRootResponseMono);
-        Mono<List<RssItem>> rssItem = rssService.getRssItem(rssRootResponseMono);
-        rssService.saveRssChannel(rssChannel).subscribe();
-        rssService.saveRssImage(rssImage).subscribe();
-        rssService.saveRssItem(rssItem).subscribe();
-        return "true";
-    }
+//    @GetMapping("/save")
+//    public String saveRssData() {
+//        Mono<RssRootResponse> rssRootResponseMono = rssService.convertXMLToPOJO(rssService.getRssXML());
+//        Mono<RssChannel> rssChannel = rssService.getRssChannel(rssRootResponseMono);
+//        Mono<RssImage> rssImage = rssService.getRssImage(rssRootResponseMono);
+//        Mono<List<RssItem>> rssItem = rssService.getRssItem(rssRootResponseMono);
+//        rssService.saveRssChannel(rssChannel).subscribe();
+//        rssService.saveRssImage(rssImage).subscribe();
+//        rssService.saveRssItem(rssItem).subscribe();
+//        return "true";
+//    }
 
     @GetMapping("/read")
     public Flux<RssData> read() {
